748ee6b39666fba26c0a4a5e0b69b64a9508a75f
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-03-29 17:07:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ const products = [
   { id: 5, name: "Product 5", price: 50 },
 ];
 
+let cartList = document.querySelector("#cart-list");
+
+let cartProducts = [];
+
 // DOM elements
 const productList = document.getElementById("product-list");
 
@@ -16,23 +20,43 @@ const productList = document.getElementById("product-list");
 function renderProducts() {
   products.forEach((product) => {
     const li = document.createElement("li");
-    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
+    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" onclick="addToCart(${product.id})" data-id="${product.id}">Add to Cart</button>`;
     productList.appendChild(li);
   });
 }
 
+renderProducts();
+
 // Render cart list
-function renderCart() {}
+function renderCart() {
+	cartList.innerHTML = "";
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+	products.forEach((product)=>{
+		if(product.id === productId){
+			cartProducts.push(product);
+			sessionStorage.setItem("cart-products", JSON.stringify(cartProducts));
+			cartList.innerHTML += 
+				`
+				 <li>${product.name} - $${product.price}</li>
+			   `
+		};
+	});
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(productId) {
+	
+}
 
 // Clear cart
-function clearCart() {}
+function clearCart() {
+	cartProducts = [];
+	sessionStorage.setItem("cart-products", "");
+	renderCart();
+}
 
 // Initial render
-renderProducts();
 renderCart();
